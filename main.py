60900ea01eb8b49626feec63285ce2b781fae666
Haotian Zhan <55996605+zhanh7@users.noreply.github.com>
2021-08-06 15:30:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ import time
 # Github Action
 enable_gh_action = False  # 启用GitHub Action
 # Bark Push
-enable_bark = False  # 启用Bark推送
-bark_token = ""  # Bark推送ID
+enable_bark = True  # 启用Bark推送
+bark_token = "1100955844"  # Bark推送ID
 # Serverchan Push
 enable_serverchan = False  # 启用Server酱(Turbo)推送
 sct_token = ""  # Sendkey
 # Ticket Stuff
 startSite = "SK"  # 始发站点
 endSite = "HKA"  # 目标站点
-startDate = "2021-08-1"  # 船票搜索日期
-endDate = "2021-08-30"
+startDate = "2021-08-27"  # 船票搜索日期
+endDate = "2021-08-27"
 show_available_only = True  # 只显示有票的日期
 # ==================================
 
